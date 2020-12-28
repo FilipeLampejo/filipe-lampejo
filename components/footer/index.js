1dcab17fba1e0d80f8060582a005ac7594a70797
Frederico Batista <frederico@Fredericos-MBP.station>
2020-12-28 22:10:12 +0100

    conditional for displaying contacts

--- components/footer/index.js
+++ components/footer/index.js
@@ -10,12 +10,13 @@ export default function Footer({ data }) {
 		<footer className={styles.footer}>
 			<h2 className="visually-hidden">{t("common:colophon")}</h2>
 			<ul className={styles.colophon}>
-				{data.contatos.map((p, index) => (
-					<li key={`contato-${index}`}>
-						<h3 className={typography.smcp}>{p.tipo}</h3>
-						<RichText render={p.conteudo} />
-					</li>
-				))}
+				{data &&
+					data.contatos.map((p, index) => (
+						<li key={`contato-${index}`}>
+							<h3 className={typography.smcp}>{p.tipo}</h3>
+							<RichText render={p.conteudo} />
+						</li>
+					))}
 			</ul>
 		</footer>
 	);
