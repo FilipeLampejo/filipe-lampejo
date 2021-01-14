b5a5d70b8315bf7e0d3fd22344ab4af269fbf465
Frederico Batista <frederico@DESKTOP-PSTEPDA.station>
2021-01-14 13:19:42 +0100

    font caching and preloading

--- components/layout/index.js
+++ components/layout/index.js
@@ -3,12 +3,17 @@ import Navbar from "../navbar";
 import { useEffect } from "react";
 import { getProjects } from "../../utils/fetcher";
 import { useRouter } from "next/router";
+import Head from "next/head";
 
 export default function Layout({ children, footer, altLangs }) {
 	const { locale } = useRouter();
 	useEffect(() => getProjects(locale), []);
 	return (
 		<>
+			<Head>
+				<link rel="preload" href="/fonts/Antwerp-Light.woff" />
+				<link rel="preload" href="/fonts/Antwerp-LightItalic.woff" />
+			</Head>
 			<Navbar altLangs={altLangs} />
 			<main>{children}</main>
 			{footer && <Footer data={footer.data} />}
--- next.config.js
+++ next.config.js
@@ -10,4 +10,17 @@ module.exports = {
 		domains: ["images.prismic.io"],
 		deviceSizes: [320, 640, 750, 828, 1080, 1200, 1920, 2048],
 	},
+	async headers() {
+		return [
+			{
+				source: "/:path*{/fonts}?",
+				headers: [
+					{
+						key: "Cache-Control",
+						value: "public,max-age=31536000,immutable",
+					},
+				],
+			},
+		];
+	},
 };
