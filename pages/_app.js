91153850a54c846cc5387892a1accea82033e08f
carolina-oa <carolina.oa.almeida@gmail.com>
2022-03-06 13:55:08 -0300

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,10 @@ import "../styles/globals.scss";
 import ReactGA from 'react-ga';
 
 ReactGA.initialize('UA-221231072-1');
-ReactGA.pageview('/');
+if (typeof window !== 'undefined') { 
+	ReactGA.set({ page: window.location.pathname }); 
+	ReactGA.pageview(window.location.pathname); 
+} 
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
