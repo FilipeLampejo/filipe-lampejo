bb502b95df5445695412820b02c193e9e45e442a
carolina-oa <carolina.oa.almeida@gmail.com>
2022-03-06 13:35:01 -0300

    Google Analytics

--- components/meta/index.js
+++ components/meta/index.js
@@ -91,6 +91,15 @@ export default function Meta({ pageTitle, pageDesc, pageType, pageImage }) {
 			<link rel="icon" href="/favicon.svg" />
 			<link rel="mask-icon" href="/favicon.svg" color="#000000" />
 
+			<script async src="https://www.googletagmanager.com/gtag/js?id=G-TB2GCYR7WW"></script>
+			<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-TB2GCYR7WW');
+			</script>
+			
 		</Head>
 	);
 }
