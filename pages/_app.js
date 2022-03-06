44a311f5692b0044ddea9cefd3db42b9b5b1c306
carolina-oa <carolina.oa.almeida@gmail.com>
2022-03-06 13:40:55 -0300

    main

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import "../styles/globals.scss";
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('G-TB2GCYR7WW');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
