ede15cbab75175413a3c87ef06f1b395af2a0587
carolina-oa <carolina.oa.almeida@gmail.com>
2022-03-06 13:49:17 -0300

    Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,8 @@ import "../styles/globals.scss";
 
 import ReactGA from 'react-ga';
 
-const history = createHistory()
 ReactGA.initialize('UA-221231072-1');
-history.listen((location, action) => {
-    ReactGA.pageview(location.pathname + location.search);
-    console.log(location.pathname)
-});
+ReactGA.pageview('/');
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
